765fb8daf7d027b9868fda43eed130754e6bf001
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-06-03 00:48:05 +0300

    Remove old RedSockSDK usage

--- POSTerminal/AppDelegate.swift
+++ POSTerminal/AppDelegate.swift
@@ -17,9 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     Fabric.with([Crashlytics.self])
-    
-    NSURLProtocol.registerClass(RedSocketURLProtocol)
-    
     return true
   }
 
