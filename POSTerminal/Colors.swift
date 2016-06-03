33cfe482021eb3f79e41159f4736ab7f8449a388
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-06-03 17:31:39 +0300

    Add app colors

---
+++ POSTerminal/Colors.swift
@@ -0,0 +1,34 @@
+//
+//  Colors.swift
+//  POSTerminal
+//
+//  Created by Dan Shevlyuk on 03/06/2016.
+//  Copyright © 2016 BinaryBlitz. All rights reserved.
+//
+
+extension UIColor {
+  
+  static func elementsAndH1Color() -> UIColor {
+    return UIColor(red:0.99, green:0.55, blue:0.22, alpha:1.0)
+  }
+  
+  static func h2Color() -> UIColor {
+    return UIColor(red:0.75, green:0.42, blue:0.17, alpha:1.0)
+  }
+  
+  static func h3Color() -> UIColor {
+    return UIColor(red:0.99, green:0.66, blue:0.67, alpha:1.0)
+  }
+  
+  static func h4Color() -> UIColor {
+    return UIColor(red:0.40, green:0.40, blue:0.48, alpha:1.0)
+  }
+  
+  static func h5Color() -> UIColor {
+    return UIColor(red:0.76, green:0.81, blue:0.85, alpha:1.0)
+  }
+  
+  static func shadowColor() -> UIColor {
+    return UIColor(red:0.67, green:0.80, blue:0.92, alpha:1.0)
+  }
+}
