be525653c90be982ec38530bac518856bfd15cb8
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-06-12 02:03:16 +0300

    Add code field to client

--- POSTerminal/Client.swift
+++ POSTerminal/Client.swift
@@ -4,6 +4,7 @@ import SwiftyJSON
 
 class Client: Object {
   dynamic var id: String = ""
+  dynamic var code: String = ""
   dynamic var name: String = ""
   dynamic var balance: Int = 0
   
@@ -16,12 +17,13 @@ extension Client: ServerObject {
   
   static func createWith(json: JSON) -> Client? {
     guard let id = json["clientRef"].string, name = json["clientName"].string,
-        balance = json["balance"].int else {
+        balance = json["balance"].int, code = json["clientCode"].string else {
       return nil
     }
     
     return {
       $0.id = id
+      $0.code = code
       $0.name = name
       $0.balance = balance
       return $0
