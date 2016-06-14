b64a925cb83528a403ef96ad5243beef8fc71154
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-06-14 10:00:49 +0300

    Add outlets for buttons

--- POSTerminal/CashPaymentViewController.swift
+++ POSTerminal/CashPaymentViewController.swift
@@ -1,11 +1,23 @@
 import UIKit
 
 class CashPaymentViewController: UIViewController {
+  
+  @IBOutlet weak var firstCashButton: UIButton!
+  @IBOutlet weak var secondCashButton: UIButton!
+  @IBOutlet weak var thirdCashButton: UIButton!
+  @IBOutlet weak var fourthCashButton: UIButton!
+  
+  let bills = [50, 100, 500, 1000, 5000]
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
     view.backgroundColor = UIColor.clearColor()
+    
+    setUpButtons()
+  }
+  
+  func setUpButtons() {
   }
   
 }
