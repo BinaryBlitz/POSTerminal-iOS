db899d0ceefb5035d499676ecde2e2780b8a4815
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-06-24 23:48:29 +0300

    Add staging eq base

--- POSTerminal/AppDelegate.swift
+++ POSTerminal/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
     Settings.loadFormUserDefaults()
     Settings.sharedInstance.wpBase = Host(baseURL: "http://arma.ngslab.ru:28081/WPServ", login: "I.Novikov", password: "123456789")
-//    Settings.sharedInstance.equipServ = Host(baseURL: "http://arma.ngslab.ru:28081/EquipServ", login: "", password: "")
+    Settings.sharedInstance.equipServ = Host(baseURL: "http://arma.ngslab.ru:28081/EquipServ", login: "", password: "")
 //    ClientManager.currentClient = Client(id: "afcb9338-0892-11e6-93fd-525400643a93", code: "381", name: "Стол 3", balance: 32000)
     
     let userDefaults = NSUserDefaults.standardUserDefaults()
